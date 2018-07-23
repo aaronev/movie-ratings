e8f761c83869fcb7aac22dcf5cf2a8f7a2407173
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-07-22 20:59:44 -0700

    completes mvp

--- js/index.js
+++ js/index.js
@@ -1,16 +1,28 @@
 $('body').append(`
   <h1>Movie Ratings</h1>
   <form>
-    <label>Movie: </label><input placeholder="Movie Title"></input>
+    <label>Movie: </label><input id="movie" placeholder="Movie Title"></input>
     <label>Rating:</label>
     <select>
-      <option value="1Star">1 Star</option>
-      <option value="2Star">2 Star</option>
-      <option value="3Star">3 Star</option>
-      <option value="4Star">4 Star</option>
-      <option value="5Star">5 Star</option>
+      <option value="1 Star">1 Star</option>
+      <option value="2 Star">2 Star</option>
+      <option value="3 Star">3 Star</option>
+      <option value="4 Star">4 Star</option>
+      <option value="5 Star">5 Star</option>
    </select>
+   <button id="submit">Submit</button>
   </form>
 `)
 
-$()
+
+$('form').on('submit', function(e) {
+  e.preventDefault()
+  var movie = $('#movie').val()
+  var rating = $('select').val()
+  $('body').append(`
+    <div>
+      <h3>${movie}</h3>
+      <h4>${rating}</h4>
+    </div>
+  `)
+})
