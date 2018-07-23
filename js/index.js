b0344b3663bd1699e103d815025a9d53b7507f5c
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-07-22 20:49:59 -0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$('body').append(`
+  <h1>Movie Ratings</h1>
+  <form>
+    <label>Movie: </label><input placeholder="Movie Title"></input>
+    <label>Rating:</label>
+    <select>
+      <option value="1Star">1 Star</option>
+      <option value="2Star">2 Star</option>
+      <option value="3Star">3 Star</option>
+      <option value="4Star">4 Star</option>
+      <option value="5Star">5 Star</option>
+   </select>
+  </form>
+`)
+
+$()
